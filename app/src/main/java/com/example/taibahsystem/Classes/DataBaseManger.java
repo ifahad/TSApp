18eb786acdfaedd6724a8e4f8f3deabc5c7f3ddc
qshaden <im3gax@gmail.com>
2017-04-11 07:45:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/taibahsystem/Classes/DataBaseManger.java
@@ -0,0 +1,8 @@
+package com.example.taibahsystem.Classes;
+
+/**
+ * Created by Manee44 on 14/03/17.
+ */
+
+public class DataBaseManger {
+}
---
+++ app/src/main/java/com/example/taibahsystem/Classes/Login.java
@@ -0,0 +1,9 @@
+package com.example.taibahsystem.Classes;
+
+/**
+ * Created by Manee44 on 14/03/17.
+ */
+
+public class Login extends DataBaseManger {
+
+}
